aa3a0b1ea73fccfed0c063847704b0b12fb8b013
Ic3 <chitrathep.c@codium.co>
2020-09-09 14:26:15 +0700

    Add Test case

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -2,111 +2,31 @@
 // Sample outputs
 //
 
-import { printScore, decideGameWinner, updateGameScore } from "./main"
+import { calculateRoman } from "./main"
 
 
-describe('Player 1', () => {
-  it('should win the game when the score is 4-0', () => {
+describe('NO.1', () => {
+  it('XXII', () => {
     // given
-    const player1Points = 4;
-    const player2Points = 0;
+    const roman1 = 'XX';
+    const roman2 = 'II';
 
     // when
-    const game = updateGameScore(player1Points, player2Points);
-    const winner = decideGameWinner(game);
+    const ans = calculateRoman(roman1, roman2);
 
     // then
-    expect(winner).toEqual('Player 1');
+    expect(ans).toEqual('XXII');
   });
 
-  it('should win the game when the score is 5-3', () => {
+  it('IV', () => {
     // given
-    const player1Points = 5;
-    const player2Points = 3;
+    const roman1 = 'II';
+    const roman2 = 'II';
 
     // when
-    const game = updateGameScore(player1Points, player2Points);
-    const winner = decideGameWinner(game);
+    const ans = calculateRoman(roman1, roman2);
 
     // then
-    expect(winner).toEqual('Player 1');
-  });
-});
-
-describe('Player 2', () => {
-  it('should win the game when the score is 4-0', () => {
-    // given
-    const player1Points = 0;
-    const player2Points = 4;
-
-    // when
-    const game = updateGameScore(player1Points, player2Points);
-    const winner = decideGameWinner(game);
-
-    // then
-    expect(winner).toEqual('Player 2');
-  });
-
-  it('should win the game when the score is 3-5', () => {
-    // given
-    const player1Points = 3;
-    const player2Points = 5;
-
-    // when
-    const game = updateGameScore(player1Points, player2Points);
-    const winner = decideGameWinner(game);
-
-    // then
-    expect(winner).toEqual('Player 2');
-  });
-});
-
-describe('Game', () => {
-  it('should print thirty - thirty when the score is 2-2', () => {
-    // given
-    const player1Points = 2;
-    const player2Points = 2;
-
-    // when
-    const score = printScore([player1Points, player2Points]);
-
-    // then
-    expect(score).toEqual('thirty - thirty');
-  });
-
-  it('should print deuce when the score is 3-3', () => {
-    // given
-    const player1Points = 3;
-    const player2Points = 3;
-
-    // when
-    const score = printScore([player1Points, player2Points]);
-
-    // then
-    expect(score).toEqual('deuce');
-  });
-
-  it('should print advantage player 1 when the score is 4-3', () => {
-    // given
-    const player1Points = 4;
-    const player2Points = 3;
-
-    // when
-    const score = printScore([player1Points, player2Points]);
-
-    // then
-    expect(score).toEqual('advantage player 1');
-  });
-
-  it('should print advantage player 2 when the score is 5-6', () => {
-    // given
-    const player1Points = 5;
-    const player2Points = 6;
-
-    // when
-    const score = printScore([player1Points, player2Points]);
-
-    // then
-    expect(score).toEqual('advantage player 2');
+    expect(ans).toEqual('IV');
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 //
 // Your implementations
 // 
+export const calculateRoman = (roman1: String, roman2: String) => {}
